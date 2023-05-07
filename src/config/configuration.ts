3035de439289165833269f6906813b73d5b49329
osmelpm <opmilan95@gmail.com>
2023-05-07 16:12:42 -0400

    Modify environment variables

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 export default () => ({
   stage: process.env.STAGE || 'dev',
-  server_host: process.env.SERVER_HOST || 'localhost',
-  server_port: +process.env.SERVER_PORT,
+  api_host: process.env.SERVER_HOST,
+  port: +process.env.PORT,
   db_host: process.env.DB_HOST,
   db_port: +process.env.POSTGRES_PORT,
   db_password: process.env.POSTGRES_PASSWORD,
